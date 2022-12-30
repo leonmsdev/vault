5ff67e35d2c35f7fe7151c6aa676a58b016f8c51
leonmsdev <leonms.dev@gmail.com>
2022-12-30 21:36:28 +0100

    refactor():add spacing

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'firebase/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
